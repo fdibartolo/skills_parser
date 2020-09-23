a4faad0baf12274504c55ccb7d5b5894c660d0d2
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2020-09-23 20:23:29 -0300

    ability to create output file

--- lib/utils.ex
+++ lib/utils.ex
@@ -1,8 +1,12 @@
 defmodule Utils do
+  @output_file "./output.json"
+
   def get_files(path, extension \\ ".xlsx") do
     path 
     |> File.ls
     |> elem(1) 
     |> Enum.filter(fn(f) -> String.ends_with?(f, extension) end)
   end
+
+  def create_output_file(content), do: File.write(@output_file, content)
 end
--- test/utils_test.exs
+++ test/utils_test.exs
@@ -1,8 +1,23 @@
 defmodule UtilsTest do
   use ExUnit.Case
 
-  test "should return only excel files" do
-    assert Utils.get_files("./test/data") |> Enum.member?("file.xlsx")
-    refute Utils.get_files("./test/data") |> Enum.member?("another.docx")
+  describe "get files" do
+    test "should return only excel files" do
+      assert Utils.get_files("./test/data") |> Enum.member?("file.xlsx")
+      refute Utils.get_files("./test/data") |> Enum.member?("another.docx")
+    end
+  end
+
+  describe "create output file" do
+    test "should create file" do
+      Utils.create_output_file("")
+      assert File.exists? "./output.json"
+    end
+
+    test "should save file content" do
+      content = "foo bar"
+      Utils.create_output_file(content)
+      assert File.read("./output.json") == {:ok, content}
+    end
   end
 end
