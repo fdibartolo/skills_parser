33c3ab8ae613ddaa02ea07fa7538a40b388609a2
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2020-09-23 16:01:29 -0300

    test to grab only valid files

--- lib/utils.ex
+++ lib/utils.ex
@@ -2,7 +2,7 @@ defmodule Utils do
   def get_files, do: get_files(File.cwd)
   def get_files({:ok, path}), do: get_files(path)
 
-  def get_files(path, extension \\ ".xls") do
+  def get_files(path, extension \\ ".xlsx") do
     path 
     |> File.ls
     |> elem(1) 
--- test/utils_test.exs
+++ test/utils_test.exs
@@ -2,7 +2,12 @@ defmodule UtilsTest do
   use ExUnit.Case
 
   test "should use current dir when no path is given" do
-    assert Utils.get_files() == []
+    assert Utils.get_files() |> Enum.empty?
+  end
+
+  test "should return only excel files" do
+    assert Utils.get_files("./test/data") |> Enum.member?("file.xlsx")
+    refute Utils.get_files("./test/data") |> Enum.member?("another.docx")
   end
 
 end
