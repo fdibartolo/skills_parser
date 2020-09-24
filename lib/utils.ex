cdb19dcbbbb943b0d24df88b90d395ce6383b514
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2020-09-24 12:24:39 -0300

    include abs path for files to parse

--- lib/utils.ex
+++ lib/utils.ex
@@ -5,7 +5,8 @@ defmodule Utils do
     path 
     |> File.ls
     |> elem(1) 
-    |> Enum.filter(fn(f) -> String.ends_with?(f, extension) end)
+    |> Enum.filter(fn f -> String.ends_with?(f, extension) end)
+    |> Enum.map(fn f -> Path.join(path, f) end)
   end
 
   def create_output_file(content), do: File.write(@output_file, content)
--- test/utils_test.exs
+++ test/utils_test.exs
@@ -3,8 +3,14 @@ defmodule UtilsTest do
 
   describe "get files" do
     test "should return only excel files" do
-      assert Utils.get_files("./test/data") |> Enum.member?("file.xlsx")
-      refute Utils.get_files("./test/data") |> Enum.member?("another.docx")
+      files = Utils.get_files("./test/data")
+      assert files |> Enum.count == 1
+      assert files |> Enum.any?(fn f -> String.contains?(f, "file.xlsx") end)
+      refute files |> Enum.any?(fn f -> String.contains?(f, "another.docx") end)
+    end
+
+    test "should include absolute path" do
+      assert Utils.get_files("./test/data") |> Enum.member?("./test/data/file.xlsx")
     end
   end
 
