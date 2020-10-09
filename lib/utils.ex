b28e8df770d1d23b8e0676bb0894f4433a5baa7b
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2020-10-09 16:34:33 -0300

    refactor syntax sugar

--- lib/utils.ex
+++ lib/utils.ex
@@ -3,16 +3,16 @@ defmodule Utils do
     path 
     |> File.ls
     |> elem(1) 
-    |> Enum.filter(fn f -> String.ends_with?(f, extension) end)
-    |> Enum.map(fn f -> Path.join(path, f) end)
+    |> Enum.filter(&String.ends_with?(&1, extension))
+    |> Enum.map(&Path.join(path, &1))
   end
 
   def create_file(content, name), do: File.write(name, content)
 
   def purge(list) do
     list
-    |> Enum.map(fn s -> String.trim s end)
-    |> Enum.reject(fn s -> s == "" end)
-    |> Enum.reject(fn s -> !String.match?(s, ~r/.,\d/) end)
+    |> Enum.map(&String.trim/1)
+    |> Enum.reject(&(&1 == ""))
+    |> Enum.reject(&!String.match?(&1, ~r/.,\d/))
   end
 end
