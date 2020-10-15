e0535a43f51af596d302d09f70f7d84ba7168005
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2020-10-15 18:18:02 -0300

    implement drildown builder

--- lib/builders/drilldown_builder.ex
+++ lib/builders/drilldown_builder.ex
@@ -1,2 +1,94 @@
 defmodule DrilldownBuilder do
+  @valid_areas_and_skills %{
+    "Containers" => ["Docker / Docker Swarm", "Openshift", "Kubernetes (standalone)", "AWS ecosystem (ECS, EKS, Fargate)", "Google Cloud ecosystem (Registry, GKE)", "Azure ecosystem (AKS, Service Fabric)"],
+    "Development" => [".Net", "Java", "Javascript/NodeJS", "Ruby", "Python"],
+    "IaC" => ["Ansible", "CloudFormation", "Chef", "Terraform", "Puppet"],
+    "Orchestrators" => ["Jenkins", "Azure DevOps pipelines", "AWS stack (CodeBuild, CodePipeline, CodeDeploy)", "Google Cloud Build", "Spinnaker", "TeamCity"],
+    "Scripting" => ["Bash", "Powershell", "Ruby", "Python"],
+    "SourceControl" => ["Git", "Mercurial", "SVN", "CVS"]
+  }
+
+  @experiences ~w(Desconozco Familiarizado Usado Experto)
+
+  @pretty_print %{ "IaC" => "Infra as Code", "SourceControl" => "Source Control" }
+
+  def build([], acc), do: acc |> group_by_capability
+  def build([set|sets], acc), do: build(sets, acc ++ [build(set)])
+  defp build(set) do
+    s = set |> normalize
+    Map.new(name: s.capability, categories: aggregate_areas(s.areas, []))
+  end
+
+  def normalize(set) do
+    normalized_areas = @valid_areas_and_skills
+      |> Map.keys
+      |> Enum.reduce([], fn a, acc -> acc ++ [find(a, Enum.find(set.areas, fn ar -> ar.area == a end))] end) 
+    Map.new(name: set.name, capability: set.capability, areas: normalized_areas)
+  end
+
+  defp find(area_name, nil), do: %{area: area_name, skills: []}
+  defp find(area_name, area), do: %{area: area_name, skills: area.skills}
+    
+  def aggregate_areas([], acc), do: acc
+  def aggregate_areas([area|areas], acc), do: aggregate_areas(areas, acc ++ [aggregate_areas(area)])
+
+  def aggregate_areas(%{area: area, skills: skills}) do
+    dataset = group_skills(area, skills)
+      |> Enum.reduce([], fn {_k,v}, acc -> [v] ++ acc end)
+      |> Utils.transpose
+      |> Enum.with_index
+      |> Enum.map(&%{data: elem(&1,0) |> Enum.reverse, label: @experiences |> Enum.at(elem(&1,1))})
+
+    Map.new(
+      name: @pretty_print |> Map.get(area, area),
+      labels: @valid_areas_and_skills |> Map.get(area) |> Enum.sort, 
+      dataset: dataset
+    )
+  end
+
+  def group_skills(area, skills) do
+    skills_to_map = skills |> Enum.reduce(%{}, fn x, acc -> Map.merge(x, acc) end)
+    skills_name = @valid_areas_and_skills |> Map.get(area)
+    experience_by_skill = skills_name |> Enum.reduce([], fn f, acc -> [Map.get(skills_to_map,f,0)] ++ acc end) |> Enum.reverse
+    accumulator = skills_name |> Enum.reduce(%{}, fn x, a -> Map.merge(%{x => [0,0,0,0]}, a) end)
+
+    skills_name
+    |> Enum.reduce(accumulator, fn s, acc -> 
+      Map.update!(acc, s, fn cv -> 
+        add_experience(Enum.at(experience_by_skill, Enum.find_index(skills_name, &(&1==s))), cv) end) end)
+  end
+
+  def add_experience(experience, acc) do
+    sum = case experience do
+      0 -> [1,0,0,0] 
+      1 -> [0,1,0,0]
+      2 -> [0,0,1,0]
+      3 -> [0,0,0,1]
+    end
+    [sum] ++ [acc] |> Utils.transpose |> Utils.reduce
+  end
+
+  def group_by_capability(list) do
+    list
+    |> Enum.group_by(&(&1.name))
+    |> Enum.map(fn {k,v} -> %{name: k, responses: Enum.count(v), categories: v |> aggregate_categories } end)
+  end
+
+  defp aggregate_categories(list) do
+    list
+    |> Enum.map(&(&1.categories)) |> List.flatten 
+    |> Enum.group_by(&(&1.name))
+    |> Enum.map(fn {a,d} -> aggregate_datasets(a,d) end)
+    |> Enum.map(fn {a,d,l} -> %{name: a, labels: l, dataset: d} end)
+  end
+
+  defp aggregate_datasets(area, list) do
+    datasets=list
+    |> Enum.reduce([], fn f, acc -> acc ++ f.dataset end)
+    |> Enum.group_by(&(&1.label))
+    |> Enum.map(fn {l,d} -> {l, d |> Enum.map(&(&1.data)) |> Utils.transpose |> Utils.reduce} end)
+    |> Enum.map(fn {l,d} -> %{data: d, label: l} end)
+
+    {area, datasets, list |> List.first |> Map.fetch!(:labels)}
+  end
 end
--- test/builders/drilldown_builder_test.exs
+++ test/builders/drilldown_builder_test.exs
@@ -1,7 +1,146 @@
 defmodule DrilldownBuilderTest do
   use ExUnit.Case
 
-  test "tbd" do
-    assert true == true
+  describe "add experience" do
+    test "should add to second level (Familiarizado)" do
+      assert DrilldownBuilder.add_experience(1, [3,1,5,7]) == [3,2,5,7]
+    end
+
+    test "should add to fourth level (Experto)" do
+      assert DrilldownBuilder.add_experience(3, [3,1,5,7]) == [3,1,5,8]
+    end
+  end
+
+  describe "group skills" do
+    test "should merge skills experience" do
+      assert DrilldownBuilder.group_skills("IaC", [%{"Ansible" => 1}, %{"Chef" => 2}, %{"Puppet" => 3}]) == %{
+        "Ansible" => [0,1,0,0],
+        "CloudFormation" => [1,0,0,0],
+        "Chef" => [0,0,1,0],
+        "Terraform" => [1,0,0,0],
+        "Puppet" => [0,0,0,1]
+      }
+    end
+  end
+
+  describe "aggregate areas" do
+    test "should merge data for a single dataset" do
+      assert DrilldownBuilder.aggregate_areas(%{area: "IaC", skills: [%{"Chef" => 2}, %{"Puppet" => 3}]}) == %{
+        name: "Infra as Code",
+        labels: ["Ansible","Chef","CloudFormation","Puppet","Terraform"], #sorted alphabetically!!
+        dataset: [
+          %{data: [1,0,1,0,1], label: "Desconozco"},
+          %{data: [0,0,0,0,0], label: "Familiarizado"},
+          %{data: [0,1,0,0,0], label: "Usado"},
+          %{data: [0,0,0,1,0], label: "Experto"}
+        ]
+      }
+    end
+
+    test "should merge data for a multiple dataset" do
+      input = [
+        %{area: "IaC", skills: [%{"Chef" => 3}, %{"Puppet" => 3}]},
+        %{area: "Scripting", skills: [%{"Python" => 1}, %{"Ruby" => 3}]},
+        %{area: "SourceControl", skills: [%{"Git" => 1}]}
+      ]
+      assert DrilldownBuilder.aggregate_areas(input, []) == [
+        %{
+          name: "Infra as Code",
+          labels: ["Ansible","Chef","CloudFormation","Puppet","Terraform"], #sorted alphabetically!!
+          dataset: [
+            %{data: [1,0,1,0,1], label: "Desconozco"},
+            %{data: [0,0,0,0,0], label: "Familiarizado"},
+            %{data: [0,0,0,0,0], label: "Usado"},
+            %{data: [0,1,0,1,0], label: "Experto"}
+          ]
+        },
+        %{
+          name: "Scripting",
+          labels: ["Bash", "Powershell", "Python", "Ruby"], #sorted alphabetically!!
+          dataset: [
+            %{data: [1,1,0,0], label: "Desconozco"},
+            %{data: [0,0,1,0], label: "Familiarizado"},
+            %{data: [0,0,0,0], label: "Usado"},
+            %{data: [0,0,0,1], label: "Experto"}
+          ]
+        },
+        %{
+          name: "Source Control",
+          labels: ["CVS", "Git", "Mercurial", "SVN"], #sorted alphabetically!!
+          dataset: [
+            %{data: [1,0,1,1], label: "Desconozco"},
+            %{data: [0,1,0,0], label: "Familiarizado"},
+            %{data: [0,0,0,0], label: "Usado"},
+            %{data: [0,0,0,0], label: "Experto"}
+          ]
+        }
+      ]
+    end
+  end
+
+  describe "normalize" do
+    test "should include missing areas" do
+      assert DrilldownBuilder.normalize(%{name: "foo", capability: "bar", areas: [%{area: "IaC", skills: [%{"Chef" => 3}]}, %{area: "Scripting", skills: [%{"Ruby" => 3}]}]})
+        |> Map.fetch!(:areas)
+        |> Enum.map(&(&1.area)) == ["Containers","Development","IaC","Orchestrators","Scripting","SourceControl"]
+    end
+    test "should include all areas" do
+      assert DrilldownBuilder.normalize(%{name: "foo", capability: "bar", areas: []})
+        |> Map.fetch!(:areas)
+        |> Enum.map(&(&1.area)) == ["Containers","Development","IaC","Orchestrators","Scripting","SourceControl"]
+    end
+  end
+
+  describe "build" do
+    test "should handle empty list" do
+      assert DrilldownBuilder.build([], []) == []
+    end
+
+    test "should include capability name" do
+      assert DrilldownBuilder.build([%{name: "foo", capability: "bar", areas: []}], [])
+        |> List.first |> Map.fetch!(:name) == "bar"
+    end
+
+    test "should aggregate skills by area in single dataset" do
+      input = [%{areas: [%{area: "IaC", skills: [%{"Ansible" => 1}, %{"Chef" => 2}, %{"Puppet" => 3}]}], capability: "A", name: "foo"}]
+      assert DrilldownBuilder.build(input,[]) |> List.first
+        |> Map.fetch!(:categories) |> Enum.find(&(&1.name == "Infra as Code")) == %{
+          name: "Infra as Code",
+          labels: ["Ansible", "Chef", "CloudFormation", "Puppet", "Terraform"],
+          dataset: [
+            %{ data: [0,0,1,0,1], label: "Desconozco" },
+            %{ data: [1,0,0,0,0], label: "Familiarizado" },
+            %{ data: [0,1,0,0,0], label: "Usado" },
+            %{ data: [0,0,0,1,0], label: "Experto" }
+          ]}
+    end
+
+    test "should include all valid and pretty-printed categories" do
+      assert DrilldownBuilder.build([%{name: "foo", capability: "bar", areas: []}],[])
+        |> List.first |> Map.fetch!(:categories)
+        |> Enum.map(&(Map.fetch!(&1,:name))) == ["Containers","Development","Infra as Code","Orchestrators","Scripting","Source Control"]
+    end
+
+    test "should include number of responses per capability" do
+      sets = [%{name: "foo", capability: "A", areas: []}, %{name: "bar", capability: "A", areas: []}, %{name: "baz", capability: "B", areas: []}]
+      result = DrilldownBuilder.build(sets, [])
+      assert result |> Enum.count == 2
+      assert result |> Enum.find(&(&1.name == "A")) |> Map.fetch!(:responses) == 2
+      assert result |> Enum.find(&(&1.name == "B")) |> Map.fetch!(:responses) == 1
+    end
+
+    test "should group response data per capability" do
+      sets = [%{name: "foo", capability: "A", areas: []}, %{name: "bar", capability: "A", areas: []}, %{name: "baz", capability: "B", areas: []}]
+      result = DrilldownBuilder.build(sets, [])
+      assert result |> Enum.find(&(&1.name == "A")) |> Map.fetch!(:categories) |> Enum.find(&(&1.name == "Infra as Code")) == %{
+        name: "Infra as Code",
+        labels: ["Ansible", "Chef", "CloudFormation", "Puppet", "Terraform"],
+        dataset: [
+          %{ data: [2,2,2,2,2], label: "Desconozco" },
+          %{ data: [0,0,0,0,0], label: "Familiarizado" },
+          %{ data: [0,0,0,0,0], label: "Usado" },
+          %{ data: [0,0,0,0,0], label: "Experto" }
+        ]}
+    end
   end
 end
