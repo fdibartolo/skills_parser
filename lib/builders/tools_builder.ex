23d82d1dd8f8879d21c76071ee4ba4600359eeed
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2020-10-19 20:23:10 -0300

    build list of all valid tools

--- lib/builders/tools_builder.ex
+++ lib/builders/tools_builder.ex
@@ -1,7 +1,8 @@
 defmodule ToolsBuilder do
 
   def build(sets) do
-    Map.new(tools: [], labels: [], dataset: build_dataset(sets, []))
+    t = Utils.valid_areas_and_skills |> build_tools
+    Map.new(tools: t, labels: [], dataset: build_dataset(sets, []))
   end
   def build_dataset([], acc), do: acc
   def build_dataset([set|sets], acc), do: build_dataset(sets, acc ++ [build_dataset(set)])
@@ -9,4 +10,9 @@ defmodule ToolsBuilder do
     Map.new()
   end
 
+  def build_tools(list) do
+    list
+    |> Enum.reduce([], fn {k,v}, acc -> acc ++ [Enum.map(v, &("#{k} - #{&1}"))] end)
+    |> List.flatten 
+  end
 end
--- test/builders/tools_builder_test.exs
+++ test/builders/tools_builder_test.exs
@@ -10,4 +10,11 @@ defmodule ToolsBuilderTest do
       assert ToolsBuilder.build([]) |> Map.keys == [:dataset, :labels, :tools]
     end
   end
+
+  describe "list of tools" do
+    test "should combine category and list into single list" do
+      list = %{"foo" => ~w(t1 t2), "bar" => ~w(t1)}
+      assert ToolsBuilder.build_tools(list) == ["bar - t1", "foo - t1", "foo - t2"]
+    end
+  end
 end
